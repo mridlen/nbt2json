a2acf8311526992af6488ff5f36761162df6aad7
Mark Ridlen <mark@ridlen.net>
2023-06-02 19:43:59 +0000

    Java and Bedrock edition level.dat both are working

--- json2nbt.sh
+++ json2nbt.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 json=$(cat $1)
+edition=$(echo $json | jq -r '.[].Edition')
+echo "Edition: $edition"
 
 process_entry() {
   # a jq formatted path like '.[0].Payloads[32].Payloads'
@@ -22,9 +24,12 @@ process_entry() {
       label=$(echo -n "$(echo $json | jq -r "$indentation_level[$i].Label" | tr -d '\n' | xxd -ps)")
       label_chars=$(( $(echo -n "$label" | wc -c) / 2 ))
       label_chars_hex="$(printf "%04x" $label_chars)"
-      label_chars_hex_be=$(echo $label_chars_hex | fold -w2 | tac | tr -d '\n')
-      #echo "Label Chars: $label_chars Hex: $label_chars_hex BigEndian: $label_chars_hex_be"
-      echo -n "$label_chars_hex_be"
+      label_chars_hex_le=$(echo $label_chars_hex | fold -w2 | tac | tr -d '\n')
+      if [[ $edition == "Bedrock" ]]; then
+        echo -n "$label_chars_hex_le"
+      elif [[ $edition == "Java" ]]; then
+        echo -n "$label_chars_hex"
+      fi
       echo -n "$label"
     # if the label length is 0, just output "0000" for null label and move on...
     else
@@ -35,7 +40,11 @@ process_entry() {
     # Payload(s)
     case $(echo $json | jq -r "$indentation_level[$i].Tag") in
       "01"|"02"|"03"|"04"|"05"|"06")
-        payload=$(echo $json | jq -r "$indentation_level[$i].Payload" | fold -w2 | tac | tr -d '\n')
+        if [[ $edition == "Bedrock" ]]; then
+          payload=$(echo $json | jq -r "$indentation_level[$i].Payload" | fold -w2 | tac | tr -d '\n')
+        elif [[ $edition == "Java" ]]; then
+          payload=$(echo $json | jq -r "$indentation_level[$i].Payload")
+        fi
         echo -n "$payload"
         ;;
       "08")
@@ -47,15 +56,23 @@ process_entry() {
             payload_chars=$(echo -n "$payload" | wc -c)
           fi
           payload_chars_hex="$(printf "%04x" $payload_chars)"
-          payload_chars_hex_be=$(echo $payload_chars_hex | fold -w2 | tac | tr -d '\n')
+          payload_chars_hex_le=$(echo $payload_chars_hex | fold -w2 | tac | tr -d '\n')
         else
-          payload_chars_hex_be="0000"
+          payload_chars_hex_le="0000"
         fi
-        echo -n "$payload_chars_hex_be"
+
+        if [[ $edition == "Bedrock" ]]; then
+          echo -n "$payload_chars_hex_le"
+        elif [[ $edition == "Java" ]]; then
+          echo -n "$payload_chars_hex"
+        fi
+
         if [[ -n $payload ]]; then
           if [[ $(echo $json | jq -r "$indentation_level[$i].TrailingNewLine") == "yes" ]]; then
+            # echo adds a trailing new line by default
             echo "$payload" | xxd -ps | tr -d '\n' 
           else
+            # echo -n means no trailing new line
             echo -n "$payload" | xxd -ps | tr -d '\n'
           fi
         fi
@@ -69,18 +86,83 @@ process_entry() {
         # next value is the number of tags
         num_payloads=$(echo $json | jq -r "$indentation_level[$i].Payloads | length")
         num_payloads_hex=$(printf "%08x" $num_payloads)
-        num_payloads_hex_be=$(echo $num_payloads_hex | fold -w2 | tac | tr -d '\n')
-        echo -n "$num_payloads_hex_be"
+        num_payloads_hex_le=$(echo $num_payloads_hex | fold -w2 | tac | tr -d '\n')
+        if [[ $edition == "Bedrock" ]]; then
+          echo -n "$num_payloads_hex_le"
+        elif [[ $edition == "Java" ]]; then
+          echo -n "$num_payloads_hex"
+        fi
 
         #echo "Payloads: $num_payloads DONE"
         local j=0
         for ((j=0; j<num_payloads; j++))
         do
-          payload=$(echo $json | jq -r "$indentation_level[$i].Payloads[$j].Payload" | fold -w2 | tac | tr -d '\n')
+          # payload 08 string has a length for each payload
+          if [[ $payload_tag == "08" ]]; then
+            payload_string_length=$(echo $json | jq -r "$indentation_level[$i].Payloads[$j].Payload" | tr -d '\n' |  wc -c)
+            payload_string_length_hex=$(printf "%04x" $payload_string_length)
+            payload_string_length_hex_le=$(echo $payload_string_length_hex | fold -w2 | tac | tr -d '\n')
+            # first 2 bytes are payload length
+            if [[ $edition == "Bedrock" ]]; then
+              echo -n "$payload_string_length_hex_le"
+            elif [[ $edition == "Java" ]]; then
+              echo -n "$payload_string_length_hex"
+            fi
+            # then output the payload
+            payload=$(echo $json | jq -r "$indentation_level[$i].Payloads[$j].Payload // empty")
+            echo -n "$payload" | xxd -ps | tr -d '\n'
+          elif [[ $payload_tag == "0a" ]]; then
+            # payload payload tag
+            payload_payload_tag=$(echo $json | jq -r "$indentation_level[$i].Payloads[$j].PayloadTag")
+            echo -n "$payload_payload_tag"
+            # label length
+            payload_label=$(echo $json | jq -r "$indentation_level[$i].Payloads[$j].Label")
+            payload_label_length=$(echo -n "$payload_label" | wc -c)
+            payload_label_length_hex=$(printf "%04x" $payload_label_length)
+            payload_label_length_hex_le=$(echo $payload_label_length_hex | fold -w2 | tac | tr -d '\n')
+            if [[ $edition == "Bedrock" ]]; then
+              echo -n "$payload_label_length_hex_le"
+            elif [[ $edition == "Java" ]]; then
+              echo -n "$payload_label_length_hex"
+            fi
+            # label
+            echo -n "$payload_label" | xxd -ps | tr -d '\n'
+            # payload
+            payload=$(echo $json | jq -r "$indentation_level[$i].Payloads[$j].Payload")
+            echo -n "$payload"
+          # all other tag types are handled this other way
+          else
+            if [[ $edition == "Bedrock" ]]; then
+              payload=$(echo $json | jq -r "$indentation_level[$i].Payloads[$j].Payload" | fold -w2 | tac | tr -d '\n')
+            elif [[ $edition == "Java" ]]; then
+              payload=$(echo $json | jq -r "$indentation_level[$i].Payloads[$j].Payload")
+            fi
+            echo -n "$payload"
+          fi
+        done
+        ;;
+      "07"|"0b"|"0c")
+        num_payloads=$(echo $json | jq -r "$indentation_level[$i].Payloads | length")
+        # length of number of payloads is 8 hex - 4 byte int
+        num_payloads_hex=$(printf "%08x" $num_payloads)
+        num_payloads_hex_le=$(echo $num_payloads_hex | fold -w2 | tac | tr -d '\n')
+        if [[ $edition == "Bedrock" ]]; then
+          echo -n "$num_payloads_hex_le"
+        elif [[ $edition == "Java" ]]; then
+          echo -n "$num_payloads_hex"
+        fi
+
+        local j=0
+        for ((j=0; j<num_payloads; j++))
+        do
+          if [[ $edition == "Bedrock" ]]; then
+            payload=$(echo $json | jq -r "$indentation_level[$i].Payloads[$j].Payload" | fold -w2 | tac | tr -d '\n')
+          elif [[ $edition == "Java" ]]; then
+            payload=$(echo $json | jq -r "$indentation_level[$i].Payloads[$j].Payload")
+          fi
           echo -n "$payload"
         done
         ;;
-        
       "0a")
         next_level="$indentation_level[$i].Payloads"
         process_entry "$next_level"
@@ -91,11 +173,32 @@ process_entry() {
   done
 }
 
+echo "Processing (this may take a few minutes)..."
+
 process_entry "." > $1.hex
-total_chars=$(($(cat $1.hex | wc -c) / 2 ))
-echo "Total Chars: $total_chars"
-total_chars_hex="$(printf "%08x" $total_chars)"
-echo "Total Chars Hex: $total_chars_hex"
-total_chars_hex_be=$(echo $total_chars_hex | fold -w2 | tac | tr -d '\n')
-echo "Big Endian: $total_chars_hex_be"
-echo -n "0a000000$total_chars_hex_be$(cat $1.hex)" | xxd -r -p > $1.compiled.dat
+if [[ $edition == "Bedrock" ]]; then
+  echo "Bedrock Edition: adding 8 byte header..."
+  total_chars=$(($(cat $1.hex | wc -c) / 2 ))
+  echo "Total Chars: $total_chars"
+  total_chars_hex="$(printf "%08x" $total_chars)"
+  echo "Total Chars Hex: $total_chars_hex"
+  total_chars_hex_le=$(echo $total_chars_hex | fold -w2 | tac | tr -d '\n')
+  echo "Little Endian: $total_chars_hex_le"
+  # bedrock has an 8 byte header
+  # first 4 bytes are:  0a 00 00 00 (0a compound open, 00 00 <null label>, 00 close)
+  # next 4 bytes are:   xx xx xx xx (this is the total bytes, little endian format)
+  # the number of bytes DOES NOT INCLUDE THE 8 BYTE HEADER!
+  echo -n "0a000000$total_chars_hex_le$(cat $1.hex)" | xxd -r -p > $1.compiled.dat
+elif [[ $edition == "Java" ]]; then
+  # java edition has an extra closing tag for some reason
+  echo "Java Edition: adding '00' footer..."
+  echo -n "$(cat $1.hex)00" | xxd -r -p > $1.compiled.dat.unzipped
+  echo "Java Edition: gzip compression..."
+  # java edetion gzips the file as well
+  gzip -cvfn $1.compiled.dat.unzipped > $1.compiled.dat
+  # just for 100% file parity, we change the gzip header OS code to "unknown"
+  echo "Java Edition: Changing gzip OS code to FF (unknown OS)..."
+  printf '\xff' | dd of=$1.compiled.dat bs=1 seek=9 count=1 conv=notrunc
+fi
+
+echo "Processing finished."
--- nbt2json.sh
+++ nbt2json.sh
@@ -3,19 +3,45 @@
 # usage:
 # nbtparse level.dat
 
+# first determine if the file is zipped or not
+gunzip -S .dat -l $1
+gzip_status=$?
+
 # this should take a level.dat and translate it into space separated hex characters
 # e.g. "00 01 02 03"
-data=$(xxd -ps $1 | tr -d "\n")
+if [[ $gzip_status -eq "1" ]]; then
+  echo "File is plain text. No extraction necessary."
+  data=$(xxd -ps $1 | tr -d "\n")
+else
+  echo "File is gzipped. Extracting file..."
+  data=$(zcat $1 | xxd -ps | tr -d "\n")
+fi
 outfile="$1.json"
-echo "$outfile"
-# 
-file_payload_size_hex="${data:14:2}${data:12:2}${data:10:2}${data:8:2}"
-file_payload_size=$(echo "ibase=16; ${file_payload_size_hex^^}" | bc)
-file_length=$(( ${#data} / 2 ))
-echo "File Payload Size Reported: $file_payload_size"
-echo "File Payload Size Actual: $(( $file_length - 8 ))"
-# the file payload size is defined by the first 8 characters but does not include them as part of the count
-echo "Difference (should be 0): $(( $file_length - $file_payload_size - 8 ))"
+echo "Output JSON file: $outfile"
+#
+# if the level.dat begins with 0a000000 - this is Bedrock
+# if the level.dat begins with 0a00000a - this is Java
+if [[ ${data:0:8} == "0a000000" ]]; then
+  echo "Header indicates Bedrock: ${data:0:8}"
+  edition="Bedrock"
+elif [[ ${data:0:8} == "0a00000a" ]]; then
+  echo "Header indicates Java: ${data:0:8}"
+  edition="Java"
+else
+  echo "Error: unrecognized level.dat edition: ${data:0:8}"
+  exit 1
+fi
+
+# Bedrock has an 8 byte header with the payload bytes, but Java starts right into the code
+if [[ $edition == "Bedrock" ]]; then
+  file_payload_size_hex="${data:14:2}${data:12:2}${data:10:2}${data:8:2}"
+  file_payload_size=$(echo "ibase=16; ${file_payload_size_hex^^}" | bc)
+  file_length=$(( ${#data} / 2 ))
+  echo "File Payload Size Reported: $file_payload_size"
+  echo "File Payload Size Actual: $(( $file_length - 8 ))"
+  # the file payload size is defined by the first 8 characters but does not include them as part of the count
+  echo "Difference (should be 0): $(( $file_length - $file_payload_size - 8 ))"
+fi
 
 payload=""
 tag=""
@@ -25,10 +51,24 @@ label=""
 payload_hex=""
 payload_length_hex=""
 payload_length=""
+payload_string_length=""
+payload_string=""
+num_payloads=""
+num_payloads_hex=""
 
 echo "[" > $outfile
-# start reading at character 16 (the 17th character since 0 is the first) - the 9th hex character
-i=16
+
+# Start reading the code at character 16 or 0, depending on edition
+if [[ $edition == "Bedrock" ]]; then
+  i=16
+  # little endian
+  be=0
+elif [[ $edition == "Java" ]]; then
+  i=0
+  # java uses big endian
+  be=1
+fi
+
 while [ $i -lt ${#data} ]; do
   echo "-"
   # read the code
@@ -41,7 +81,11 @@ while [ $i -lt ${#data} ]; do
   if [[ $tag != "00" ]]; then
     # read the label length
     # next two bytes in big endian  reverse order
-    label_length_hex="${data:i+2:2}${data:i:2}"
+    if [[ $be -eq "0" ]]; then
+      label_length_hex="${data:i+2:2}${data:i:2}"
+    elif [[ $be -eq "1" ]]; then
+      label_length_hex="${data:i:4}"
+    fi
     label_length=$(echo "ibase=16; ${label_length_hex^^}" | bc)
     #echo "Label Length: $label_length"
     ((i+=4))
@@ -52,11 +96,13 @@ while [ $i -lt ${#data} ]; do
   # read the label
   if [[ $label_length -ne "0" ]]; then
     label_hex="${data:i:$label_length * 2}"
+    echo "Label Hex: $label_hex"
     label="\"$(echo "$label_hex" | xxd -r -p)\""
     echo "Label: $label"
     ((i+=$label_length * 2))
   else
     label="null"
+    echo "Label: null"
   fi
 
   case $tag in
@@ -65,60 +111,80 @@ while [ $i -lt ${#data} ]; do
       #echo "Payload Type: byte"
       payload_hex="${data:i:2}"
       echo "Payload: ${payload_hex^^}"
-      echo -n "{ \"Tag\":\"$tag\", \"Label\":$label, \"Payload\":\"$payload_hex\" }" >> $outfile
+      echo -n "{\"Tag\":\"$tag\", \"Label\":$label, \"Payload\":\"$payload_hex\"}" >> $outfile
       ((i+=2))
       ;;
     # 02 - 2 byte short
     "02")
       #echo "Payload Type: short"
-      payload_hex="${data:i+2:2}${data:i:2}"
+      if [[ $be -eq "0" ]]; then
+        payload_hex="${data:i+2:2}${data:i:2}"
+      elif [[ $be -eq "1" ]]; then
+        payload_hex="${data:i:4}"
+      fi
       echo "Payload: ${payload_hex^^}"
-      echo -n "{ \"Tag\":\"$tag\", \"Label\":$label, \"Payload\":\"$payload_hex\" }" >> $outfile
+      echo -n "{\"Tag\":\"$tag\", \"Label\":$label, \"Payload\":\"$payload_hex\"}" >> $outfile
       ((i+=4))
       ;;
     # 03 - 4 byte int
     "03")
       #echo "Payload Type: int"
-      payload_hex="${data:i+6:2}${data:i+4:2}${data:i+2:2}${data:i:2}"
+      if [[ $be -eq "0" ]]; then
+        payload_hex="${data:i+6:2}${data:i+4:2}${data:i+2:2}${data:i:2}"
+      elif [[ $be -eq "1" ]]; then
+        payload_hex="${data:i:8}"
+      fi
       echo "Payload: ${payload_hex^^}"
-      echo -n "{ \"Tag\":\"$tag\", \"Label\":$label, \"Payload\":\"$payload_hex\" }" >> $outfile
+      echo -n "{\"Tag\":\"$tag\", \"Label\":$label, \"Payload\":\"$payload_hex\"}" >> $outfile
       ((i+=8))
       ;;
     # 04 - 8 byte long int
     "04")
       #echo "Payload Type: long"
-      payload_hex="${data:i+14:2}${data:i+12:2}${data:i+10:2}${data:i+8:2}${data:i+6:2}${data:i+4:2}${data:i+2:2}${data:i:2}"
+      if [[ $be -eq "0" ]]; then
+        payload_hex="${data:i+14:2}${data:i+12:2}${data:i+10:2}${data:i+8:2}${data:i+6:2}${data:i+4:2}${data:i+2:2}${data:i:2}"
+      elif [[ $be -eq "1" ]]; then
+        payload_hex="${data:i:16}"
+      fi
       echo "Payload: ${payload_hex^^}"
-      echo -n "{ \"Tag\":\"$tag\", \"Label\":$label, \"Payload\":\"$payload_hex\" }" >> $outfile
+      echo -n "{\"Tag\":\"$tag\", \"Label\":$label, \"Payload\":\"$payload_hex\"}" >> $outfile
       ((i+=16))
       ;;
     # 05 - 4 byte float
     "05")
       #echo "Payload Type: float"
-      payload_hex="${data:i+6:2}${data:i+4:2}${data:i+2:2}${data:i:2}"
+      if [[ $be -eq "0" ]]; then
+        payload_hex="${data:i+6:2}${data:i+4:2}${data:i+2:2}${data:i:2}"
+      elif [[ $be -eq "1" ]]; then
+        payload_hex="${data:i:8}"
+      fi
       echo "Payload: ${payload_hex^^}"
-      echo -n "{ \"Tag\":\"$tag\", \"Label\":$label, \"Payload\":\"$payload_hex\" }" >> $outfile
+      echo -n "{\"Tag\":\"$tag\", \"Label\":$label, \"Payload\":\"$payload_hex\"}" >> $outfile
       ((i+=8))
       ;;
         # 04 - 8 byte double float
     "06")
       #echo "Payload Type: double"
-      payload_hex="${data:i+14:2}${data:i+12:2}${data:i+10:2}${data:i+8:2}${data:i+6:2}${data:i+4:2}${data:i+2:2}${data:i:2}"
+      if [[ $be -eq "0" ]]; then
+        payload_hex="${data:i+14:2}${data:i+12:2}${data:i+10:2}${data:i+8:2}${data:i+6:2}${data:i+4:2}${data:i+2:2}${data:i:2}"
+      elif [[ $be -eq "1" ]]; then
+        payload_hex="${data:i:16}"
+      fi
       echo "Payload: ${payload_hex^^}"
-      echo -n "{ \"Tag\":\"$tag\", \"Label\":$label, \"Payload\":\"$payload_hex\" }" >> $outfile
+      echo -n "{\"Tag\":\"$tag\", \"Label\":$label, \"Payload\":\"$payload_hex\"}" >> $outfile
       ((i+=16))
       ;;
-    # 07 - byte array
-    "07")
-      echo "Fatal error: Tag type 07 - 'byte array' not implemented yet."
-      exit 1
-      ;;
+    
     # 08 - string
     "08")
       #echo "Payload Type: string"
 
       # read 2 byte string length
-      payload_length_hex="${data:i+2:2}${data:i:2}"
+      if [[ $be -eq "0" ]]; then
+        payload_length_hex="${data:i+2:2}${data:i:2}"
+      elif [[ $be -eq "1" ]]; then
+        payload_length_hex="${data:i:4}"
+      fi
       payload_length=$(echo "ibase=16; ${payload_length_hex^^}" | bc)
       ((i+=4))
 
@@ -150,7 +216,11 @@ while [ $i -lt ${#data} ]; do
       ((i+=2))
 
       # number of payloads in 4 byte int big endian
-      num_payloads_hex="${data:i+6:2}${data:i+4:2}${data:i+2:2}${data:i:2}"
+      if [[ $be -eq "0" ]]; then
+        num_payloads_hex="${data:i+6:2}${data:i+4:2}${data:i+2:2}${data:i:2}"
+      elif [[ $be -eq "1" ]]; then
+        num_payloads_hex="${data:i:8}"
+      fi
       num_payloads=$(echo "ibase=16; ${num_payloads_hex^^}" | bc)
       ((i+=8))
       
@@ -166,32 +236,167 @@ while [ $i -lt ${#data} ]; do
             ((i+=2))
             ;;
           "02")
-            payload_hex="${data:i+2:2}${data:i:2}"
+            if [[ $be -eq "0" ]]; then
+              payload_hex="${data:i+2:2}${data:i:2}"
+            elif [[ $be -eq "1" ]]; then
+              payload_hex="${data:i:4}"
+            fi
             ((i+=4))
             ;;
           "03"|"05")
-            payload_hex="${data:i+6:2}${data:i+4:2}${data:i+2:2}${data:i:2}"
+            if [[ $be -eq "0" ]]; then
+              payload_hex="${data:i+6:2}${data:i+4:2}${data:i+2:2}${data:i:2}"
+            elif [[ $be -eq "1" ]]; then
+              payload_hex="${data:i:8}"
+            fi
             ((i+=8))
             ;;
           "04"|"06")
-            payload_hex="${data:i+14:2}${data:i+12:2}${data:i+10:2}${data:i+8:2}${data:i+6:2}${data:i+4:2}${data:i+2:2}${data:i:2}"
+            if [[ $be -eq "0" ]]; then
+              payload_hex="${data:i+14:2}${data:i+12:2}${data:i+10:2}${data:i+8:2}${data:i+6:2}${data:i+4:2}${data:i+2:2}${data:i:2}"
+            elif [[ $be -eq "1" ]]; then
+              payload_hex="${data:i:16}"
+            fi
             ((i+=16))
             ;;  
+          "08")
+            # payload starts with a two byte short int length
+            if [[ $be -eq "0" ]]; then
+              payload_string_length_hex="${data:i+2:2}${data:i:2}"
+            elif [[ $be -eq "1" ]]; then
+              payload_string_length_hex="${data:i:4}"
+            fi
+            ((i+=4))
+            # convert the payload length from hex to decimal and multiply by two, since we need 2 hex chars for each byte
+            payload_string_length=$(( $(echo "ibase=16; ${payload_string_length_hex^^}" | bc) * 2 ))
+            payload_hex="${data:i:$payload_string_length}"
+            payload_string="$( echo "$payload_hex" | xxd -r -p )"
+            ((i+=$payload_string_length))
+            ;;
+          "0a")
+            # 0a payload tag indicates that the payloads are typed and labeled
+            # However, unlike a tag 0a, there is no closing 00 tag end
+            # Example Data:
+            # 09 00 0a 41 74 74 72 69 62 75 74 65 73 0a 00 00 00 01 06 00 04 42 61 73 65 3f b9 99 99 a0 00 00 00
+            #
+            # Tag: 09
+            # Label Length: 00 0a - 10 chars
+            # Label: Attributes
+            # Payload Tag: 0a
+            # Num Payloads: 00 00 00 01 - 1 payload
+            # ------------------------------------- This code portion focuses on the last half:
+            #   Payload Tag: 06
+            #   Label Length: 00 04
+            #   Label: 42 61 73 65 - Base
+            #   Payload: 3f b9 99 99 a0 00 00 00
+
+            payload_payload_tag="${data:i:2}"
+            echo "Payload Payload Tag: $payload_payload_tag"
+            ((i+=2))
+            
+            # payload starts with a two byte short int length
+            if [[ $be -eq "0" ]]; then
+              payload_string_length_hex="${data:i+2:2}${data:i:2}"
+            elif [[ $be -eq "1" ]]; then
+              payload_string_length_hex="${data:i:4}"
+            fi
+            ((i+=4))
+
+            # convert the payload length from hex to decimal and multiply by two, since we need 2 hex chars for each byte
+            payload_string_length=$(( $(echo "ibase=16; ${payload_string_length_hex^^}" | bc) * 2 ))
+            payload_label_hex="${data:i:$payload_string_length}"
+            payload_string="$( echo "$payload_label_hex" | xxd -r -p )"
+            echo "Payload Label: $payload_string"
+            ((i+=$payload_string_length))
+
+            # take the payload based on the payload_payload_tag
+            case $payload_payload_tag in
+              "01")
+                payload="${data:i:2}"
+                ((i+=2))
+                ;;
+              "02")
+                payload="${data:i:4}"
+                ((i+=4))
+                ;;
+              "03"|"05")
+                payload="${data:i:8}"
+                ((i+=8))
+                ;;
+              "04"|"06")
+                payload="${data:i:16}"
+                ((i+=16))
+                ;;
+            esac
+            if [[ $be -eq "0" ]]; then
+              payload_hex=$(echo -n "$payload" | fold -w2 | tac | tr -d '\n')
+            elif [[ $be -eq "1" ]]; then
+              payload_hex=$payload
+            fi
+            ;;
         esac
         echo "Payload: $payload_hex"
-        echo -n "{\"Payload\":\"$payload_hex\"}" >> $outfile
+        
+        # output string payloads to ascii text
+        if [[ $payload_tag == "08" ]]; then
+          echo -n "{\"Payload\":\"$payload_string\"}" >> $outfile
+        elif [[ $payload_tag == "0a" ]]; then
+          echo -n "{\"PayloadTag\":\"$payload_payload_tag\", \"Label\":\"$payload_string\", \"Payload\":\"$payload_hex\"}" >> $outfile
+        else
+          echo -n "{\"Payload\":\"$payload_hex\"}" >> $outfile
+        fi
+
         if [[ $c -lt $num_payloads ]]; then
           echo -n "," >> $outfile
         fi
       done
       echo -n "] }" >> $outfile
       ;;
-    # 0b - ??
-    # 0c - ??
-    "0b"|"0c")
-      echo "Fatal Error: tags 0b and 0c not implemented yet."
-      exit 1
-      ;;
+  # 07 - byte array
+  # 0b - int array
+  # 0c - long array
+  "07"|"0b"|"0c")
+    # number of payloads - first 4 bytes
+    if [[ $be -eq "0" ]]; then
+      num_payloads_hex="${data:i+6:2}${data:i+4:2}${data:i+2:2}${data:i:2}"
+    elif [[ $be -eq "1" ]]; then
+      num_payloads_hex="${data:i:8}"
+    fi
+    ((i+=8))
+    num_payloads=$(echo "ibase=16; ${num_payloads_hex^^}" | bc)
+    echo "Num Payloads: $num_payloads"
+    echo -n "{\"Tag\":\"$tag\", \"Label\":$label, \"Payloads\": [" >> $outfile
+    for (( c=0; c<$num_payloads; c++ ))
+    do
+      case $tag in
+        "07")
+          payload="${data:i:2}"
+          ((i+=2))
+          ;;
+        "0b")
+          if [[ $be -eq "0" ]]; then
+            payload="${data:i+6:2}${data:i+4:2}${data:i+2:2}${data:i:2}"
+          elif [[ $be -eq "1" ]]; then
+            payload="${data:i:8}"
+          fi
+          ((i+=8))
+          ;;
+        "0c")
+          if [[ $be -eq "0" ]]; then
+            payload="${data:i+14:2}${data:i+12:2}${data:i+10:2}${data:i+8:2}${data:i+6:2}${data:i+4:2}${data:i+2:2}${data:i:2}"
+          elif [[ $be -eq "1" ]]; then
+            payload="${data:i:16}"
+          fi
+          ((i+=16))
+          ;;
+      esac
+      echo -n "{\"Payload\":\"$payload\"}" >> $outfile
+      if [[ $c -lt $(( $num_payloads -1 )) ]]; then
+        echo -n "," >> $outfile
+      fi
+    done
+    echo -n "] }" >> $outfile
+    ;;
   esac
 
   # this part is for the nested tags
@@ -199,13 +404,18 @@ while [ $i -lt ${#data} ]; do
     # 0a - compound tag
     "0a")
       echo "Compound Tag Open"
-      echo -n "{\"Tag\":\"$tag\", \"Label\":$label, \"Payloads\": [  " >> $outfile
+      echo -n "{\"Tag\":\"$tag\", \"Edition\":\"$edition\", \"Label\":$label, \"Payloads\": [  " >> $outfile
       ;;
     "00")
       echo "Compound Tag Close"
       # remove the trailing comma
       truncate --size=-2 $outfile
-      echo -n "] }" >> $outfile
+      if [[ $edition == "Java" && $i == ${#data} ]]; then
+        # do nothing - Java edition seems to put in an extra closing tag
+        :
+      else
+        echo -n "] }" >> $outfile
+      fi
       ;;
   esac
 
